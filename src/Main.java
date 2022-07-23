809020d96eefa0208682ed3ec7dcacf2a466107a
Pruthvi Kolhe <65055563+pruthvikx@users.noreply.github.com>
2022-07-23 17:09:46 +0530

    Sorted Array challenge in Java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+        int[] mySortedArray = SortedArray.getIntegers(5);
+        int[] sorted = SortedArray.sortIntegers(mySortedArray);
+        SortedArray.printArray(sorted);
+    }
+
+}
